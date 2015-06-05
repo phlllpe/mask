57e05e6c1ea6782d97967071af9f1190ca714abb
philipe <philipe@voxtecnologia.com.br>
2015-06-05 09:44:44 -0300

    #030 - create brazilian cpf test

--- tests/Mask/Predefined/Br/CpfTest.php
+++ tests/Mask/Predefined/Br/CpfTest.php
@@ -2,30 +2,30 @@
 
 namespace Tests\Mask\Predefined\Br;
 
-use Mask\Predefined\Br\Cnpj;
+use Mask\Predefined\Br\Cpf;
 use PHPUnit_Framework_TestCase;
 
-class CnpjTest extends PHPUnit_Framework_TestCase
+class CpfTest extends PHPUnit_Framework_TestCase
 {
     use \Tests\Mask\Structure\SetUp;
 
     public function testGetStringMask() 
     {
-        $this->assertContains('.', (new Cnpj)->getStringMask());
-        $this->assertContains('/', (new Cnpj)->getStringMask());
-        $this->assertNotContains('\\', (new Cnpj)->getStringMask());
-        $this->assertContains('#', (new Cnpj)->getStringMask());
-        $this->assertContains('-', (new Cnpj)->getStringMask());
+        $this->assertContains('.', (new Cpf)->getStringMask());
+        $this->assertNotContains('/', (new Cpf)->getStringMask());
+        $this->assertNotContains('\\', (new Cpf)->getStringMask());
+        $this->assertContains('#', (new Cpf)->getStringMask());
+        $this->assertContains('-', (new Cpf)->getStringMask());
     }
     
     public function provider()
     {
         return array(
-            array('65787304000196'),
-            array('6578730400019'),
-            array('657873040001'),
-            array('65787304000'),
-            array('6578730400'),
+            array('05265403496'),
+            array('0526540349'),
+            array('052654034'),
+            array('05265403'),
+            array('0526540'),
         );
     }
 
@@ -33,32 +33,32 @@ class CnpjTest extends PHPUnit_Framework_TestCase
     /**
      * @dataProvider provider
      */
-    public function testMaskDefault($cnpj) 
+    public function testMaskDefault($cpf) 
     {
-        $this->assertContains('-', (new Cnpj)->mask($cnpj));
-        $this->assertTrue(strlen($cnpj) < strlen((new Cnpj)->mask($cnpj)));
+        $this->assertContains('-', (new Cpf)->mask($cpf));
+        $this->assertTrue(strlen($cpf) < strlen((new Cpf)->mask($cpf)));
     }
 
     /**
      * @dataProvider provider
      */
-    public function testMaskCompleteWith($cnpj) 
+    public function testMaskCompleteWith($cpf) 
     {
-        $this->assertContains('-', (new Cnpj)->mask($cnpj, '0', STR_PAD_RIGHT));
-        $this->assertTrue(strlen((new Cnpj)->mask($cnpj)) <= strlen((new Cnpj)->mask($cnpj, '0', STR_PAD_RIGHT)));
-        $this->assertTrue(strlen((new Cnpj)->mask($cnpj)) === strlen((new Cnpj)->mask($cnpj, ' ')));
+        $this->assertContains('-', (new Cpf)->mask($cpf, '0', STR_PAD_RIGHT));
+        $this->assertTrue(strlen((new Cpf)->mask($cpf)) <= strlen((new Cpf)->mask($cpf, '0', STR_PAD_RIGHT)));
+        $this->assertTrue(strlen((new Cpf)->mask($cpf)) === strlen((new Cpf)->mask($cpf, ' ')));
     }
     
     public function testMaskReal() 
     {
-        $this->assertContains('-', (new Cnpj)->mask('41362586000111'));
-        $this->assertContains('.', (new Cnpj)->mask('41362586000111'));
-        $this->assertContains('/', (new Cnpj)->mask('41362586000111'));
-        $this->assertEquals('41.362.586/0001-11', (new Cnpj)->mask('41362586000111'));
-        $this->assertEquals('41.362.586/0000-00', (new Cnpj)->mask('41362586000', '0', STR_PAD_RIGHT));
-        $this->assertEquals('41.362.586/0000-00', (new Cnpj)->mask('41362586', '0', STR_PAD_RIGHT));
-        $this->assertEquals('41.360.000/0000-00', (new Cnpj)->mask('4136', '0', STR_PAD_RIGHT));
-        $this->assertEquals('  .   .   /  41-36', (new Cnpj)->mask('4136'));
+        $this->assertContains('-', (new Cpf)->mask('05265403496'));
+        $this->assertContains('.', (new Cpf)->mask('05265403496'));
+        $this->assertNotContains('/', (new Cpf)->mask('05265403496'));
+        $this->assertEquals('052.654.034-96', (new Cpf)->mask('05265403496'));
+        $this->assertEquals('052.654.034-90', (new Cpf)->mask('0526540349', '0', STR_PAD_RIGHT));
+        $this->assertEquals('052.654.034-00', (new Cpf)->mask('052654034', '0', STR_PAD_RIGHT));
+        $this->assertEquals('052.654.000-00', (new Cpf)->mask('052654', '0', STR_PAD_RIGHT));
+        $this->assertEquals('000.000.000-52', (new Cpf)->mask('052'));
     }
     
 }
